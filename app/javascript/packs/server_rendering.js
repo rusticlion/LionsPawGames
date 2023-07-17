6b1cd94a29d23edb4c586d44e79b104b5307b800
LeonOfTheHollow <russell.l.bates@gmail.com>
2023-07-17 13:08:41 -0400

    Still trying to point the server-side js rendering at the right CSS

--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -1,6 +1,6 @@
 // By default, this pack is loaded for server-side rendering.
 // It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
-import "../../assets/stylesheets/application.css"
+import "../stylesheets/application.scss"
 
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
