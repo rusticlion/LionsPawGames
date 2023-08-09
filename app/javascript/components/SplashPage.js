d9c263b6e19d67283e19537f956ce43038c71b2d
LeonOfTheHollow <russell.l.bates@gmail.com>
2023-08-09 16:29:18 -0400

    Falling cherry blossom animation for splash page

--- app/javascript/components/SplashPage.js
+++ app/javascript/components/SplashPage.js
@@ -1,31 +1,49 @@
 import React, { useEffect, useState } from 'react';
 
 const SplashPage = props => {
-  const [koan, setKoan] = useState(null);
+  const maxPetals = 100;
+  const batchFrequency = 30000;
 
   useEffect(() => {
-    fetch('/api/gateless-gate/daily')
-      .then(response => response.json())
-      .then(data => setKoan(data))
-      .catch(error => console.error("Couldn't fetch a koan from the API"))
-  }, []);
+    const cherryBlossoms = document.getElementById('cherry-blossoms');
 
-  const paragraphs = [];
-  if (koan?.text) {
-    koan.text.split(/\\n|\n/).forEach(line => {
-      paragraphs.push(line);
-    })
-  }
-  const mumon_lines = [];
-  if (koan?.mumon_commentary) {
-    koan.mumon_commentary.split(/\\n|\n/).forEach(line => {
-      mumon_lines.push(line);
-    })
-  }
+    for (let i = 0; i < maxPetals; i++) {
+      setTimeout(() => {
+        const petal = document.createElement('div');
+        petal.classList.add('petal');
+        cherryBlossoms.appendChild(petal);
+        
+        const startLeft = Math.random() * window.innerWidth;
+        const startTop = -20;
+        const endTop = window.innerHeight;
+
+        petal.style.left = startLeft + 'px';
+        petal.style.top = startTop + 'px';
+
+        const animatePetal = () => {
+          petal.style.top = parseInt(petal.style.top) + 1 + 'px';
+          petal.style.left = parseInt(petal.style.left) + (Math.random() * 2 - 1) + 'px';
+
+          if (parseInt(petal.style.left) < 0) {
+            cherryBlossoms.removeChild(petal);
+          } else if (parseInt(petal.style.top) < endTop) {
+            requestAnimationFrame(animatePetal);
+          } else {
+            cherryBlossoms.removeChild(petal);
+          }
+        };
+
+        requestAnimationFrame(animatePetal);
+      }, Math.random() * 5000);
+    }
+  }, []);
 
   return (
     <div>
-      Welcome. Please, feel free to wander.
+      <div id="cherry-blossoms"></div>
+      <p>
+        Welcome. Please, feel free to wander.
+      </p>
     </div>
   )
 };
