c08c19d432524b706e85351b0807402330d8c98d
LeonOfTheHollow <russell.l.bates@gmail.com>
2023-08-16 18:20:03 -0400

    Removed empty pages from navbar

--- app/javascript/components/NavigationOverlay.js
+++ app/javascript/components/NavigationOverlay.js
@@ -15,8 +15,8 @@ const NavigationOverlay = () => {
           <li><a href="/zen-garden">Zen Garden</a></li>
           <li><a href="/quote-me">Quote Me</a></li>
           <li><a href="/games">Games</a></li>
-          <li><a href="/essays">Essays</a></li>
-          <li><a href="/reviews">Reviews</a></li>
+          {/* <li><a href="/essays">Essays</a></li>
+          <li><a href="/reviews">Reviews</a></li> */}
           <li><a href="/resume">Résumé</a></li>
         </ul>
         <div id="close-button" onClick={toggleOverlay}>Close</div>
