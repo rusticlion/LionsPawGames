906582d5cb8b9e2638aec9cebe41e3739424cd64
LeonOfTheHollow <russell.l.bates@gmail.com>
2023-08-16 13:40:04 -0400

    Fixed font loading issue on certain platforms

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,17 +2,5 @@ const { environment } = require('@rails/webpacker')
 const sassLoader = require('./loaders/sass.js')
 
 environment.loaders.prepend('sass', sassLoader)
-environment.loaders.append('fonts', {
-  test: /\.(woff|woff2|eot|ttf|otf)$/,
-  use: [
-    {
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]',
-        outputPath: 'fonts/',
-      },
-    },
-  ],
-});
 
 module.exports = environment
