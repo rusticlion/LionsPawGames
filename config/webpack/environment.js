4d60e62bb1c98d2a0d0e65865e3297433dcb52ec
LeonOfTheHollow <russell.l.bates@gmail.com>
2024-08-19 17:54:52 -0500

    webpack tweak

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,4 +8,6 @@ if (environment.loaders) {
   console.warn('environment.loaders is undefined. Skipping sass loader configuration.')
 }
 
+environment.config.set('node', false)
+
 module.exports = environment
